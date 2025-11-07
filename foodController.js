71c900b83b96d182189bddcf52a2c398f69d2ae9
Saloni <s69056298@gmail.com>
2025-11-07 23:47:39 +0530

    Update foodController.js

--- foodController.js
+++ foodController.js
@@ -1,20 +1,14 @@
-// controllers/foodController.js
-
 const Food = require('../models/Food');
-
 exports.getFoods = async (req, res) => {
     const foods = await Food.find();
     res.json(foods);
 };
-
 exports.addFood = async (req, res) => {
     const { name, price, description, image } = req.body;
     const food = new Food({ name, price, description, image });
     await food.save();
     res.json({ msg: 'Food added successfully!' });
 };
-
-
 exports.addMultipleFoods = async (req, res) => {
     try {
         await Food.insertMany(req.body);
@@ -23,4 +17,5 @@ exports.addMultipleFoods = async (req, res) => {
         console.error(err);
         res.status(500).json({ msg: 'Error adding foods' });
     }
+
 };
