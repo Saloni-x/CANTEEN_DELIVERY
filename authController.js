db0e393908ad43703f5abb8c83b218902163250e
Saloni <s69056298@gmail.com>
2025-11-07 23:46:53 +0530

    Update authController.js

--- authController.js
+++ authController.js
@@ -4,26 +4,22 @@ const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
     try {
-        const { name, email, password, role } = req.body; // role bhi lelo
-
+        const { name, email, password, role } = req.body;
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ msg: 'Email already exists!' });
         }
-
         const hashedPassword = await bcrypt.hash(password, 10);
-
         const user = new User({
             name,
             email,
             password: hashedPassword,
-            role: role || 'customer'   // agar role nahi diya toh default 'customer'
+            role: role || 'customer'
         });
 
         await user.save();
 
         const token = jwt.sign({ id: user._id }, 'secret');
-
         res.json({ 
             msg: 'Registration successful!',
             token: token 
@@ -34,14 +30,11 @@ exports.register = async (req, res) => {
         res.status(500).json({ msg: 'Server Error' });
     }
 };
-
 exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
-
         if (!user) return res.status(400).json({ msg: 'User not found!' });
-
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials!' });
 
@@ -51,4 +44,5 @@ exports.login = async (req, res) => {
         console.error(err);
         res.status(500).json({ msg: 'Server Error' });
     }
+
 };
